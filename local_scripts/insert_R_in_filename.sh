26cbfa584b1fd2b13c6b59f21296fd9f52e13e10
simonleandergrimm <simonleandergrimm@gmail.com>
2022-11-09 14:25:58 +0100

    Adapted insert_R script to take local directory structure into account

--- local_scripts/insert_R_in_filename.sh
+++ local_scripts/insert_R_in_filename.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-#For all files in the current directory, insert an "R" after "_" in the filename
+#Quickly put-together bash script to edit all fastq file endings from *1_sequence.fastq to *_R1.fastq or *2_sequence.fastq to *_R2.fastq
+
+cd ..
+WORKDIR=$(pwd)
+SEQUENCES=$WORKDIR/sequences
+LOGS=$WORKDIR/logs
+
 {
-    for FILE in *;
+    for DIR in $SEQUENCES/*;
+
     do
-        echo "Renaming $FILE to $(echo $FILE | sed 's/_/_R/')"
-        mv $FILE $(echo $FILE | sed 's/_/_R/')
+        echo "found $DIR";
+        for FILE in $DIR/*;
+        do
+
+            if [[ $FILE == *.fastq ]];
+            then
+                echo "$FILE was recognized as a .fastq file"
+                echo "Renaming $DIR/$FILE to $(echo $DIR/$FILE | sed 's/1_sequence.fastq/R1.fastq/')"
+                mv $FILE $(echo $DIR/$FILE | sed 's/1_sequence.fastq/R1.fastq/')
+                echo "Renaming $DIR/$FILE to $(echo $DIR/$FILE | sed 's/2_sequence.fastq/R2.fastq/')"
+                mv $FILE $(echo $DIR/$FILE | sed 's/2_sequence.fastq/R2.fastq/')
+            fi
+        done
     done
-} > rename_log.txt
+} > $LOGS/insert_R_in_filename.txt
+
