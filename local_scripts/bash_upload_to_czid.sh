79b04555ba1cf3b622d80b622f836e68bea279cb
simonleandergrimm <simonleandergrimm@gmail.com>
2022-11-09 14:15:28 +0100

    Fixed metadata selection in czid upload

--- local_scripts/bash_upload_to_czid.sh
+++ local_scripts/bash_upload_to_czid.sh
@@ -2,10 +2,10 @@
 cd ..
 WORKDIR=$(pwd)
 SEQUENCES=$WORKDIR/sequences
-METADATA=$WORKDIR/metadata
+METADATA=$WORKDIR/metadata/*.csv
 
 czid short-read-mngs upload-samples \
-  -p '11_09_2022_nao_exp_4_0005' \
+  -p '11_09_2022_nao_exp_4_005' \
   --metadata-csv $METADATA \
   $SEQUENCES
 
